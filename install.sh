808515b573e6d1dfd14baf85d9a5c3b148c46329
Yaquochi <yakov.lopatin@gmail.com>
2025-07-07 22:38:41 +0300

    update script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ echo "=== Начинаем установку dotfiles и настройку Fed
 
 # Подготовка
 echo "Обновление пакетов и подключение репозиториев..."
+sudo dnf clean all
+sudo dnf makecache
 sudo dnf update -y
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 echo "=== Обновление и подключение прошло успешно ==="
